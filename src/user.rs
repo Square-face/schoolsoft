47214e5b1c7cae84538b7ed7fe3bf2d246960ebb
Square-face <linus.michelsson@gmail.com>
2024-02-08 10:38:31 +0000

    Made a token type and deserializer

--- src/user.rs
+++ src/user.rs
@@ -1,3 +1,4 @@
+use serde::de::{MapAccess, Visitor};
 use serde::Deserialize;
 use serde_repr::Deserialize_repr;
 
@@ -16,13 +17,16 @@ pub enum UserType {
 ///
 /// While a appkey never changes, a token is only valid for 3 hours after which it must be
 /// refreshed using another call to /<school>/rest/app/token.
-#[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Token {
+    /// function for getting utc::now()
+    now: fn() -> chrono::NaiveDateTime,
+
     /// The token itself
     pub token: String,
 
     /// When the token expires
-    pub expires: chrono::DateTime<chrono::Utc>,
+    pub expires: chrono::NaiveDateTime,
 }
 
 /// A schoolsoft organization
@@ -61,6 +65,7 @@ pub struct Org {
     /// Url to login to the organization using a web browser
     /// Once again, this field makes no since as you get it by logging in, so why would you need to
     /// log in again?
+    /// And no its not the url for getting the token, that is /<school>/rest/app/token
     #[serde(rename = "tokenLogin")]
     pub token_login: String,
 }
@@ -104,6 +109,156 @@ pub struct User {
     pub orgs: Vec<Org>,
 }
 
+impl Token {
+    /// Create a new token with a custom now function
+    ///
+    /// This is useful for testing as it allows you to set the current time to a fixed value
+    ///
+    /// # Example
+    /// ```
+    /// # use schoolsoft::user::Token;
+    /// let token = Token::new_with_now(
+    ///    "notrealtoken123_1337_1".to_string(),
+    ///    chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap().and_hms_opt(12, 0, 0).unwrap(),
+    ///    || chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap().and_hms_opt(11, 0, 0).unwrap(),
+    /// );
+    /// ```
+    ///
+    pub fn new_with_now(
+        token: String,
+        expires: chrono::NaiveDateTime,
+        now: fn() -> chrono::NaiveDateTime,
+    ) -> Token {
+        Token {
+            now,
+            token,
+            expires,
+        }
+    }
+
+    /// Returns the duration until the token expires
+    ///
+    /// # Example
+    /// ```
+    /// # use schoolsoft::user::Token;
+    /// # use chrono::Duration;
+    /// let token = Token::new_with_now(
+    ///     "notrealtoken123_1337_1".to_string(),
+    ///     chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap().and_hms_opt(12, 0, 0).unwrap(),
+    ///     || chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap().and_hms_opt(11, 0, 0).unwrap(),
+    ///);
+    ///
+    /// assert_eq!(token.expires_in(), Duration::hours(1));
+    /// ```
+    ///
+    pub fn expires_in(&self) -> chrono::Duration {
+        self.expires - (self.now)()
+    }
+
+    /// Check if the token has expired
+    ///
+    /// # Example
+    /// ```
+    /// # use schoolsoft::user::Token;
+    /// let token = Token::new_with_now(
+    ///    "notrealtoken123_1337_1".to_string(),
+    ///    chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap().and_hms_opt(12, 0, 0).unwrap(),
+    ///    || chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap().and_hms_opt(12, 0, 1).unwrap(),
+    /// );
+    /// assert_eq!(token.is_expired(), true);
+    /// ```
+    ///
+    pub fn is_expired(&self) -> bool {
+        self.expires < (self.now)()
+    }
+
+    /// Check if the token is still valid
+    ///
+    /// Same as inverting [Token::is_expired()]
+    ///
+    /// # Example
+    /// ```
+    /// # use schoolsoft::user::Token;
+    /// let token = Token::new_with_now(
+    ///   "notrealtoken123_1337_1".to_string(),
+    ///   chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap().and_hms_opt(12, 0, 0).unwrap(),
+    ///   || chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap().and_hms_opt(11, 0, 0).unwrap(),
+    /// );
+    /// assert_eq!(token.is_valid(), true);
+    /// ```
+    ///
+    pub fn is_valid(&self) -> bool {
+        !self.is_expired()
+    }
+}
+
+struct TokenVisitor;
+
+impl<'de> Visitor<'de> for TokenVisitor {
+    type Value = Token;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a map of strings")
+    }
+
+    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    where
+        A: MapAccess<'de>,
+    {
+        let mut token = Option::<String>::None;
+        let mut expires = Option::<chrono::NaiveDateTime>::None;
+
+        while let Some(key) = map.next_key::<String>()? {
+            match key.as_str() {
+                "token" => {
+                    token = Some(match map.next_value::<String>() {
+                        Ok(token) => token,
+                        Err(err) => return Err(err),
+                    });
+                }
+                "expiryDate" => {
+                    expires = Some(match map.next_value::<String>() {
+                        Ok(expires) => match chrono::NaiveDateTime::parse_from_str(
+                            &expires,
+                            "%Y-%m-%d %H:%M:%S%.f",
+                        ) {
+                            Ok(expires) => expires,
+                            Err(err) => return Err(serde::de::Error::custom(err)),
+                        },
+                        Err(err) => return Err(err),
+                    });
+                }
+                _ => {
+                    return Err(serde::de::Error::unknown_field(
+                        key.as_str(),
+                        &["token", "expiryDate"],
+                    ));
+                }
+            }
+        }
+
+        Ok(Token {
+            now: || chrono::Utc::now().naive_utc(),
+            token: match token {
+                Some(token) => token,
+                None => return Err(serde::de::Error::custom("Token is missing")),
+            },
+            expires: match expires {
+                Some(expires) => expires,
+                None => return Err(serde::de::Error::custom("Expires is missing")),
+            },
+        })
+    }
+}
+
+impl<'de> Deserialize<'de> for Token {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_map(TokenVisitor)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -159,8 +314,159 @@ mod tests {
                 org.token_login,
                 "https://sms1.schoolsoft.se/mock_school/jsp/app/TokenLogin.jsp?token=TOKEN_PLACEHOLDER&orgid=1&childid=1337&redirect=https%3A%2F%2Fsms1.schoolsoft.se%2mock_school%2Fjsp%2Fstudent%2Fright_student_startpage.jsp".to_string()
             );
-            
         }
     }
 
+    mod token {
+        use super::*;
+
+        /// Mock now function that returns a fixed date
+        fn now() -> chrono::NaiveDateTime {
+            chrono::NaiveDate::from_ymd_opt(2024, 1, 1)
+                .unwrap()
+                .and_hms_opt(12, 0, 0)
+                .unwrap()
+        }
+
+        #[test]
+        fn valid_triple_decimal() {
+            let token: Token = serde_json::from_str(
+                r#"{
+                    "expiryDate":"2024-01-01 12:00:00.123",
+                    "token":"123notrealtoken123_1337_1"
+                }"#,
+            )
+            .expect("Failed to deserialize JSON with 3 decimal places");
+
+            assert_eq!(token.token, "123notrealtoken123_1337_1".to_string());
+
+            assert_eq!(
+                token.expires,
+                chrono::NaiveDate::from_ymd_opt(2024, 1, 1)
+                    .unwrap()
+                    .and_hms_opt(12, 0, 0)
+                    .unwrap()
+                    + chrono::Duration::milliseconds(123)
+            );
+        }
+
+        #[test]
+        fn valid_double_decimal() {
+            let _: Token = serde_json::from_str(
+                r#"{
+                    "expiryDate":"2024-02-06 21:37:15.15",
+                    "token":"123notrealtoken123_1337_1"
+                }"#,
+            )
+            .expect("Failed to deserialize JSON with 2 decimal places");
+        }
+
+        #[test]
+        fn valid_single_decimal() {
+            let _: Token = serde_json::from_str(
+                r#"{
+                    "expiryDate":"2024-02-06 21:37:15.1",
+                    "token":"123notrealtoken123_1337_1"
+                }"#,
+            )
+            .expect("Failed to deserialize JSON with 1 decimal place");
+        }
+
+        #[test]
+        fn valid_no_decimal() {
+            let _: Token = serde_json::from_str(
+                r#"{
+                    "expiryDate":"2024-02-06 21:37:15",
+                    "token":"123notrealtoken123_1337_1"
+                }"#,
+            )
+            .expect("Failed to deserialize JSON with no decimal places");
+        }
+
+        #[test]
+        fn expiration_in_1h() {
+            let mut token: Token = serde_json::from_str(
+                r#"{
+                    "expiryDate":"2024-01-01 12:00:00",
+                    "token":"123notrealtoken123_1337_1"
+                }"#,
+            )
+            .expect("Failed to deserialize JSON");
+
+            token.now = || {
+                chrono::NaiveDate::from_ymd_opt(2024, 1, 1)
+                    .unwrap()
+                    .and_hms_opt(11, 0, 0)
+                    .unwrap()
+            };
+
+            assert_eq!(token.expires_in(), chrono::Duration::hours(1));
+        }
+
+        #[test]
+        fn expiration_in_1m() {
+            let mut token: Token = serde_json::from_str(
+                r#"{
+                    "expiryDate":"2024-01-01 12:00:00",
+                    "token":"123notrealtoken123_1337_1"
+                }"#,
+            )
+            .expect("Failed to deserialize JSON");
+
+            token.now = || {
+                chrono::NaiveDate::from_ymd_opt(2024, 1, 1)
+                    .unwrap()
+                    .and_hms_opt(11, 59, 0)
+                    .unwrap()
+            };
+
+            assert_eq!(token.expires_in(), chrono::Duration::minutes(1));
+            assert_eq!(token.is_expired(), false);
+            assert_eq!(token.is_valid(), true);
+        }
+
+        #[test]
+        fn expiration_now() {
+            let mut token: Token = serde_json::from_str(
+                r#"{
+                    "expiryDate":"2024-01-01 12:00:00",
+                    "token":"123notrealtoken123_1337_1"
+                }"#,
+            )
+            .expect("Failed to deserialize JSON");
+
+            token.now = || {
+                chrono::NaiveDate::from_ymd_opt(2024, 1, 1)
+                    .unwrap()
+                    .and_hms_opt(12, 0, 0)
+                    .unwrap()
+            };
+
+            assert_eq!(token.expires_in(), chrono::Duration::zero());
+            assert_eq!(token.is_expired(), false);
+            assert_eq!(token.is_valid(), true);
+        }
+
+        #[test]
+        fn expiration_expired() {
+            let mut token: Token = serde_json::from_str(
+                r#"{
+                    "expiryDate":"2024-01-01 12:00:00",
+                    "token":"123notrealtoken123_1337_1"
+                }"#,
+            )
+            .expect("Failed to deserialize JSON");
+
+            token.now = || {
+                chrono::NaiveDate::from_ymd_opt(2024, 1, 1)
+                    .unwrap()
+                    .and_hms_opt(12, 0, 1)
+                    .unwrap()
+            };
+
+            assert_eq!(token.expires_in(), chrono::Duration::seconds(-1));
+            assert_eq!(token.is_expired(), true);
+            assert_eq!(token.is_valid(), false);
+        }
+    }
 }
