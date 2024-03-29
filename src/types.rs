4d5f723b7bf61b27b0abd552d3fb0faa35abb9aa
Square-face <linus.michelsson@gmail.com>
2024-03-29 11:22:16 +0000

    changed <school> to \[school\] in the docs

--- src/types.rs
+++ src/types.rs
@@ -17,10 +17,10 @@ pub enum UserType {
 /// A schoolsoft token
 ///
 /// A token is used to authenticate with the schoolsoft api. It is retrieved by logging in and
-/// making a request to /<school>/rest/app/token with the app key gained from the login.
+/// making a request to /\[school\]/rest/app/token with the app key gained from the login.
 ///
 /// While a appkey never changes, a token is only valid for 3 hours after which it must be
-/// refreshed using another call to /<school>/rest/app/token.
+/// refreshed using another call to /\[school\]/rest/app/token.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Token {
     /// function for getting utc::now()
@@ -66,7 +66,7 @@ pub struct Org {
     /// Url to login to the organization using a web browser
     /// Once again, this field makes no since as you get it by logging in, so why would you need to
     /// log in again?
-    /// And no its not the url for getting the token, that is /<school>/rest/app/token
+    /// And no its not the url for getting the token, that is /\[school\]/rest/app/token
     pub token_login: String,
 }
 
@@ -94,7 +94,7 @@ pub struct User {
     /// Token used for interacting with api routes that require authentication
     ///
     /// This field is not populated by logging in. Instead it requires a separate request to
-    /// /<school>/rest/app/token with the app key.
+    /// /\[school\]/rest/app/token with the app key.
     pub token: Option<Token>,
 
     /// What type of user this is
